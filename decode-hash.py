62731c7ed687ead37e9a5690536b0e81b72ac35a
brunoorlandin <bruno.orlandin@hotmail.com>
2023-04-05 15:17:26 -0300

    added table with results

--- decode-hash.py
+++ decode-hash.py
@@ -1,10 +1,17 @@
 import hashlib
 import codecs
+import pandas as pd
+import dataframe_image as dfi
 
 f = codecs.open("list.txt", "r", "utf-8")
 text = f.read()
 texts = text.split("\n")
 texts_to_decode = {}
+table = {}
+strings = []
+sha256Hashes = []
+md5Hashes = []
+status = []
 
 for key in texts:
     keysToTexts = key.split('" - ')
@@ -13,8 +20,6 @@ for key in texts:
     texts_to_decode[keysToTexts[0]] = [valuesToTexts[0],valuesToTexts[1].strip()]
 
 for key in texts_to_decode.keys():
-
-    string = texts_to_decode[key][1]
     
     byteString = key.encode('utf-8')
 
@@ -27,7 +32,44 @@ for key in texts_to_decode.keys():
     sha256HashedString = shaHash.hexdigest()
     md5HashedString = md5Hash.hexdigest()
 
-    if sha256HashedString == texts_to_decode[key][0] and md5HashedString == texts_to_decode[key][1]:
-        print(f'key => {key}\n')
-        print(f'sha256 => {texts_to_decode[key][0]}\n')
-        print(f'md5 => {texts_to_decode[key][1]}\n')
+    if (
+        sha256HashedString == texts_to_decode[key][0] and
+        md5HashedString == texts_to_decode[key][1]
+    ):
+        # print(f'key => {key}\n')
+        # print(f'sha256 => {texts_to_decode[key][0]}\n')
+        # print(f'md5 => {texts_to_decode[key][1]}\n')
+        strings.append(key)
+        sha256Hashes.append(texts_to_decode[key][0])
+        md5Hashes.append(texts_to_decode[key][1])
+        status.append('Verdadeira')
+    else:
+        strings.append(key)
+        sha256Hashes.append(texts_to_decode[key][0])
+        md5Hashes.append(texts_to_decode[key][1])
+        status.append('Falsa')
+
+table['Frase'] = strings
+table['SHA 256 Gerado'] = sha256Hashes
+table['MD5 gerado'] = md5Hashes
+table['Status'] = status
+
+# print(table)
+
+df = pd.DataFrame(table)
+
+df.style.hide(axis='index')
+df.style.set_table_styles(
+    [
+        {
+            'selector': 'th',
+            'props': [
+                        ('background', 'green'),
+                        ('text-align', 'center')
+                    ]
+        }
+    ]
+)
+
+#only use if in unix like systems
+dfi.export(df,"mytable.png")
