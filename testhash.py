f62a3582f0fdc5dcf95354e85ccfb15ac7e01ba9
Shawn Kawabe <59843897+shawnkawabe@users.noreply.github.com>
2023-04-25 21:36:37 -0300

    add testhash

---
+++ testhash.py
@@ -0,0 +1,17 @@
+# test_with_unittest.py
+
+from unittest import TestCase
+
+class TryTesting(TestCase):
+   def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+    def test_reversed():
+        assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+    def test_some_primes():
+        assert 37 in {
+            num
+            for num in range(2, 50)
+            if not any(num % div == 0 for div in range(2, num))
+        }
