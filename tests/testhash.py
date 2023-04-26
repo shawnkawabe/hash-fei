a1c5ee1255cd11ed0e80c27b9a148e8d995f5776
Shawn Kawabe <kawabeshawn@gmail.com>
2023-04-25 22:07:30 -0300

    fix tests pipeline

--- tests/testhash.py
+++ tests/testhash.py
@@ -3,15 +3,5 @@
 from unittest import TestCase
 
 class TryTesting(TestCase):
-   def test_uppercase():
-    assert "loud noises".upper() == "LOUD NOISES"
-
-    def test_reversed():
-        assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
-
-    def test_some_primes():
-        assert 37 in {
-            num
-            for num in range(2, 50)
-            if not any(num % div == 0 for div in range(2, num))
-        }
+  def test_uppercase(self):
+    self.assertEqual( "loud noises".upper(), "LOUD NOISES")
